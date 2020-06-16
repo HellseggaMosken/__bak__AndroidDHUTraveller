f19b33577473b9b5ef9c18d45b8c026129a36dc6
HellseggaMosken <thetemp@qq.com>
2020-06-16 22:21:12 +0800

    normal update

--- app/src/main/java/dhu/cst/zhangcheng171010220/dhutraveller/GalleryActivity.java
+++ app/src/main/java/dhu/cst/zhangcheng171010220/dhutraveller/GalleryActivity.java
@@ -9,12 +9,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
 public class GalleryActivity extends AppCompatActivity {
-
     DhuBuilding building;
     ViewPager2 viewPager2;
 
@@ -25,11 +25,20 @@ public class GalleryActivity extends AppCompatActivity {
 
         final Bundle bundle = this.getIntent().getExtras();
         if (bundle == null) {
-            this.finish();
+            finish();
             return;
         }
         this.building = (DhuBuilding) bundle.getSerializable("building");
         this.viewPager2 = findViewById(R.id.gallery_viewPager);
+        TextView textViewHead = findViewById(R.id.activity_gallery_head_textView);
+        textViewHead.setText(building.name);
+        View viewBack = findViewById(R.id.activity_gallery_head_back);
+        viewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         viewPager2.setAdapter(new RecyclerView.Adapter<PictViewHolder>() {
             @NonNull
